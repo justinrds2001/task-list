9c71279d57725c1d52e5bc6b1ed70fd6b0f60798
Justin <stage@gsd.nl>
2025-01-31 14:09:33 +0100

    lesson-33

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,11 +3,23 @@
 @section('title', 'The list of tasks')
 
 @section('content')
+    <nav class="mb-4">
+        <a href="{{ route('task.create') }}" class="link">Add Task!</a>
+    </nav>
+
     @forelse ($tasks as $task)
         <div>
-            <a href="{{ route('task.show', ['id' => $task->id ]) }}">{{ $task->title }}</a>
+            <a href="{{ route('task.show', ['task' => $task ]) }}"
+                @class(['line-through' => $task->completed])>
+            {{ $task->title }}</a>
         </div>
     @empty
         <div>There are no tasks</div>
     @endforelse
+
+    @if ($tasks->count())
+        <nav class="mt-4">
+            {{ $tasks->links() }}
+        </nav>
+    @endif
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,96 +1,58 @@
 <?php
 
+use App\Http\Requests\TaskRequest;
+use App\Models\Task;
+use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Route;
-
-
-class Task
-{
-    public function __construct(
-        public int $id,
-        public string $title,
-        public string $description,
-        public ?string $long_description,
-        public bool $completed,
-        public string $created_at,
-        public string $updated_at
-    ) {
-    }
-}
-$tasks = [
-    new Task(
-        1,
-        'Buy groceries',
-        'Task 1 description',
-        'Task 1 long description',
-        false,
-        '2023-03-01 12:00:00',
-        '2023-03-01 12:00:00'
-    ),
-    new Task(
-        2,
-        'Sell old stuff',
-        'Task 2 description',
-        null,
-        false,
-        '2023-03-02 12:00:00',
-        '2023-03-02 12:00:00'
-    ),
-    new Task(
-        3,
-        'Learn programming',
-        'Task 3 description',
-        'Task 3 long description',
-        true,
-        '2023-03-03 12:00:00',
-        '2023-03-03 12:00:00'
-    ),
-    new Task(
-        4,
-        'Take dogs for a walk',
-        'Task 4 description',
-        null,
-        false,
-        '2023-03-04 12:00:00',
-        '2023-03-04 12:00:00'
-    ),
-];
 
 Route::get('/', function () {
     return redirect()->route('task.index');
 });
 
-Route::get('/tasks', function () use($tasks) {
+Route::get('/tasks', function () {
     return view('index', [
-        'tasks'=> $tasks,
+        'tasks'=> Task::latest()->paginate(10),
     ]);
 })->name('task.index');
 
+Route::view('/tasks/create', 'create')->name('task.create');
 
-Route::get('/tasks/{id}', function ($id) use($tasks) {
-    $task = collect($tasks)->firstWhere('id', $id);
+Route::get('/tasks/{task}/edit', function (Task $task) {
+    return view('edit', [
+        'task' => $task,
+    ]);
+})->name('task.edit');
 
-    if (!$task) {
-        return abort(Response::HTTP_NOT_FOUND);
-    }
-
+Route::get('/tasks/{task}', function (Task $task) {
     return view('show', [
         'task' => $task,
     ]);
 })->name('task.show');
 
-// Route::get ('/hello', function () {
-//     return 'Hello!';
-// })->name('hello');
+Route::post('/tasks', function (TaskRequest $request) {
+    $task = Task::create($request->validated());
 
-// // Route::redirect('/hallo', '/hello');
-// Route::get('/hallo', function () {
-//     return redirect()->route('hello');
-// });
+    return redirect()->route('task.show', ['task' => $task])->with('success', 'Task created successfully!');
+})->name('task.store');
 
-// Route::get ('/greet/{name}', function ($name) {
-//     return "Hello $name!";
-// });
+Route::put('/tasks/{task}', function (Task $task, TaskRequest $request) {
+    $task->update($request->validated());
+
+    return redirect()->route('task.show', ['task' => $task])->with('success', 'Task updated successfully!');
+})->name('task.update');
+
+Route::delete('/tasks/{task}', function (Task $task) {
+    $task->delete();
+
+    return redirect()->route('task.index')->with('success','Task deleted successfully!');
+})->name('task.destroy');
+
+Route::put('/tasks/{task}/togglecomplete', function(Task $task) {
+    $task->toggleComplete();
+
+    return redirect()->back()->with('success', 'Task updated successfully!');
+})->name('task.toggle-complete');
 
 Route::fallback(function () {
     return 'Page not found';
